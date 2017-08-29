2905b526ce151346e5ecfac9c38c03af08856772
Vanessa Paco <vanessapr.85@gmail.com>
2017-08-28 19:51:55 -0500

    added css for paint weekends and weekdays

--- calendar.js
+++ calendar.js
@@ -20,12 +20,12 @@
     containerMonth.className = 'calendar-month';
     // header month
     var headerMonth = document.createElement('header');
-    var headerMonthTitle = document.createElement('h1');
-    var headerText = document.createTextNode(months[this.month]);
+    var headerMonthTitle = document.createElement('h5');
+    var headerText = document.createTextNode(months[this.month] + ' ' + this.year);
     headerMonthTitle.appendChild(headerText);
     headerMonth.appendChild(headerMonthTitle);
     // header month days week
-    var daysWeek = document.createElement('header');
+    var daysWeek = document.createElement('div');
     daysWeek.className = 'week-header';
     var daysWeekString = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
     for (i = 0; i < daysWeekString.length; i++) {
@@ -48,15 +48,23 @@
 
     for (i = 1; i <= 35; i++) { //35 = 7*5
       span = document.createElement('span');
+      // days weekend: Sunday
+      if (i % 7 === 1) {
+        span.className = 'weekend-day';
+      }
 
+      // days added of the month
       index = this.days.indexOf(i-firstWeekDay);
       if (index !== -1) {
+        span.className = (i % 7 === 1)? 'weekend-day' : 'week-day';
         text = document.createTextNode(this.days[index]);
         span.appendChild(text);
       }
 
       weekMonth.appendChild(span);
       if ( i % 7 === 0) {
+        // days weekend: Saturday
+        span.className = 'weekend-day';
         containerMonth.appendChild(weekMonth);
         weekMonth = document.createElement('div');
         weekMonth.className = 'week-month';
